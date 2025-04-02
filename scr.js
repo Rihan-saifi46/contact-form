93e9d900471aac278daaefb19bebb45899d18185
Rihan-saifi46 <kharihan786k@gmail.com>
2025-04-02 15:12:08 +0530

    add

---
+++ scr.js
@@ -0,0 +1,14 @@
+VANTA.GLOBE({
+    el: ".con",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3279a6,
+    color2: 0x246b90,
+    size: 1.30,
+    backgroundColor: 0xe8e8ed
+  })
